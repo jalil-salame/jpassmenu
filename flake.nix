8980e3d544001b5fa5437a05e56ec91e1d292e35
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-01-15 16:39:24 +0100

    fix(flake): Don't depend on flake-utils

--- flake.nix
+++ flake.nix
@@ -1,44 +1,32 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  # A helpful description of your flake
+  description = "A fork of passmenu that uses fuzzel";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
-    (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        src = builtins.path { name = "jpassmenu"; path = ./jpassmenu; };
-        jpassmenu-unwrapped = pkgs.writers.writeBashBin "jpassmenu" (builtins.readFile src);
-        wrapperPath = pkgs.lib.makeBinPath [
-          pkgs.fuzzel
-          pkgs.ydotool
-          pkgs.coreutils
-          pkgs.findutils
-          pkgs.getopt
-          pkgs.git
-          pkgs.gnugrep
-          pkgs.gnupg
-          pkgs.gnused
-          pkgs.tree
-          pkgs.which
-          pkgs.openssh
-          pkgs.procps
-          pkgs.qrencode
-        ];
-        jpassmenu = jpassmenu-unwrapped.overrideAttrs (final: prev: {
-          postFixup = ''
-            wrapProgram $out/bin/passmenu \
-              --prefix PATH : "$out/bin:${wrapperPath}"
-          '';
-        });
-      in
-      {
-        packages = {
-          inherit jpassmenu;
-          default = jpassmenu;
-        };
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ jpassmenu ];
-        };
-      });
+  # Flake inputs
+  inputs.flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs, flake-schemas }:
+    let
+      # Helpers for producing system-specific outputs
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (import nixpkgs { inherit system; }));
+
+    in
+    {
+      # Schemas tell Nix about the structure of your flake's outputs
+      inherit (flake-schemas) schemas;
+
+      packages = forEachSupportedSystem (pkgs:
+        let
+          src = builtins.path { name = "jpassmenu"; path = ./jpassmenu; };
+          jpassmenu = pkgs.writers.writeBashBin "jpassmenu" (builtins.readFile src);
+        in
+        {
+          inherit jpassmenu; default = jpassmenu;
+        }
+      );
+
+      formatter = forEachSupportedSystem (pkgs: pkgs.nixpkgs-fmt);
+    };
 }
