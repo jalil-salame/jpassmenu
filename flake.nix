a721a173ae89d60712db3c017cc871a67024d408
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-01-17 00:39:13 +0100

    feat: Add overlay

--- flake.nix
+++ flake.nix
@@ -12,20 +12,17 @@
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (import nixpkgs { inherit system; }));
 
+      # Package
+      src = builtins.path { name = "jpassmenu"; path = ./jpassmenu; };
+      package = pkgs: pkgs.writers.writeBashBin "jpassmenu" (builtins.readFile src);
     in
     {
       # Schemas tell Nix about the structure of your flake's outputs
       inherit (flake-schemas) schemas;
 
-      packages = forEachSupportedSystem (pkgs:
-        let
-          src = builtins.path { name = "jpassmenu"; path = ./jpassmenu; };
-          jpassmenu = pkgs.writers.writeBashBin "jpassmenu" (builtins.readFile src);
-        in
-        {
-          inherit jpassmenu; default = jpassmenu;
-        }
-      );
+      packages = forEachSupportedSystem (pkgs: let jpassmenu = package pkgs; in { inherit jpassmenu; default = jpassmenu; });
+
+      overlays.default = final: prev: { jpassmenu = package prev; };
 
       formatter = forEachSupportedSystem (pkgs: pkgs.nixpkgs-fmt);
     };
